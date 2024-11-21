d7238a30be91be5d73b16d49d6f9266d92e8f696
Ankush7491 <ankushsharma7491@live.com>
2024-11-21 17:02:13 +0530

    basic xml configuration

--- springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
+++ springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
@@ -2,13 +2,15 @@ package ankush.spring.springcore.beans;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor
 public class Employee {
     @Getter @Setter private int employeeId;
     @Getter @Setter	private String employeeName;
-    @Getter @Setter private String employeeAddres;
+    @Getter @Setter private String employeeAddress;
 }
