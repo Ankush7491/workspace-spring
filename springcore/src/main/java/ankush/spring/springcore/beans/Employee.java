64cc8a63b36869a5c1330116652a24e294b0340f
Ankush7491 <ankushsharma7491@live.com>
2024-11-22 17:23:02 +0530

    Auto-wiring, utils collections

--- springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
+++ springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
@@ -1,5 +1,8 @@
 package ankush.spring.springcore.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,12 +10,13 @@ import lombok.Setter;
 import lombok.ToString;
 
 
+@AllArgsConstructor
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
     @Getter @Setter private int employeeId;
     @Getter @Setter	private String employeeName;
     @Getter @Setter private String employeeAddress;
     @Getter @Setter private Salary salary;
+    @Autowired @Qualifier("gurgaon") @Getter @Setter private Office office;
 }
