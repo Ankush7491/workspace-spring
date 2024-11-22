f7e667e4ffd6d99aae7d7e8047613a7f7bbb279f
Ankush7491 <ankushsharma7491@live.com>
2024-11-22 11:02:49 +0530

    xml configs

--- springcore/src/main/java/ankush/spring/springcore/App.java
+++ springcore/src/main/java/ankush/spring/springcore/App.java
@@ -1,6 +1,6 @@
 package ankush.spring.springcore;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ankush.spring.springcore.beans.Employee;
@@ -12,11 +12,18 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         String configFile="./ankush/spring/springcore/configuration/xmlConfig.xml";
-        ApplicationContext context= new ClassPathXmlApplicationContext(new String[] {configFile});
+        AbstractApplicationContext context= new ClassPathXmlApplicationContext(new String[] {configFile});
         Employee employee=(Employee) context.getBean("employee");
         System.out.println(employee.toString());
         Employee employee2=(Employee) context.getBean("employee2");
         System.out.println(employee2.toString());
+        Employee employee3=(Employee) context.getBean("ciemployee");
+        System.out.println(employee3.toString());
+        Employee employee4=(Employee) context.getBean("cemployee");
+        System.out.println(employee4.toString());
+        Employee employee5=(Employee) context.getBean("cemployee2");
+        System.out.println(employee5.toString());
+        context.registerShutdownHook();
  
     }
 }
--- springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
+++ springcore/src/main/java/ankush/spring/springcore/beans/Employee.java
@@ -6,9 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@AllArgsConstructor
+
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     @Getter @Setter private int employeeId;
     @Getter @Setter	private String employeeName;
